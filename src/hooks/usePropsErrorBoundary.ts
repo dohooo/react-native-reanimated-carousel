2450fe120b22ec561a3bc7c4b687ccfee0bfc080
赵東澔 <zhaodonghao586@outlook.com>
2021-11-18 13:31:45 +0800

    fix(hooks/usepropserrorboundary.ts): always be error when data props set null
    fix #34

--- src/hooks/usePropsErrorBoundary.ts
+++ src/hooks/usePropsErrorBoundary.ts
@@ -4,7 +4,7 @@ import type { ICarouselProps } from 'src/Carousel';
 export function usePropsErrorBoundary(props: ICarouselProps<unknown>) {
     React.useEffect(() => {
         const { defaultIndex, data } = props;
-        if (typeof defaultIndex === 'number') {
+        if (typeof defaultIndex === 'number' && data?.length > 0) {
             if (defaultIndex < 0 || defaultIndex >= data.length) {
                 throw Error(
                     'DefaultIndex must be in the range of data length.'
