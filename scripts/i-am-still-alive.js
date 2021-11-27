70ab9e5080cdce3269edcfb6875147dff2b8fc77
赵東澔 <zhaodonghao586@outlook.com>
2021-11-27 13:10:05 +0800

    chore: turn over a new leaf

--- scripts/i-am-still-alive.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const filePath = '.i-am-still-alive';
-const isExists = fs.existsSync(filePath);
-if (isExists) {
-    fs.unlinkSync(filePath);
-} else {
-    fs.writeFileSync(filePath);
-}
