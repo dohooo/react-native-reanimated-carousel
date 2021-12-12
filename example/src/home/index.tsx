9f3068bbb9dadbdd046ad50cf4437b53f335e4a9
赵東澔 <zhaodonghao586@outlook.com>
2021-12-12 15:22:26 +0800

    fix: fixed types

--- example/src/home/index.tsx
+++ example/src/home/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { RootStackParamList } from '../index';
+import type { RootStackParamList } from '../index';
 import { Colors, ListItem } from 'react-native-ui-lib';
 
 const LayoutsPage: Array<Record<'name', keyof RootStackParamList>> = [
