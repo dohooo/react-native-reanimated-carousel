fd25fb6017b25d5013c6116eee470044977f0e4f
赵東澔 <zhaodonghao586@outlook.com>
2021-12-19 15:55:04 +0800

    style: fix home styles

--- example/src/home/index.tsx
+++ example/src/home/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import type { RootStackParamList } from '../index';
 import { Colors, ListItem } from 'react-native-ui-lib';
@@ -16,7 +16,7 @@ const LayoutsPage: Array<Record<'name', keyof RootStackParamList>> = [
 const Index = () => {
     const navigation = useNavigation<NavigationProp<RootStackParamList>>();
     return (
-        <View style={{ flex: 1 }}>
+        <View style={StyleSheet.absoluteFill}>
             {LayoutsPage.map(({ name }, index) => {
                 return (
                     <ListItem
