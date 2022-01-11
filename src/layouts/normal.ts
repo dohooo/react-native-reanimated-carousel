850e96082ec44422d99c5a5a1767a6530b1405d9
XunGong <enjoygrant@163.com>
2022-01-11 19:00:41 +0800

    fix: remove ExtrapolateParameter of normal layout translate
    #96

--- src/layouts/normal.ts
+++ src/layouts/normal.ts
@@ -1,16 +1,11 @@
-import { Extrapolate, interpolate } from 'react-native-reanimated';
+import { interpolate } from 'react-native-reanimated';
 
 export function normalLayout(opts: { size: number; vertical: boolean }) {
     const { size, vertical } = opts;
 
     return (value: number) => {
         'worklet';
-        const translate = interpolate(
-            value,
-            [-1, 0, 1],
-            [-size, 0, size],
-            Extrapolate.CLAMP
-        );
+        const translate = interpolate(value, [-1, 0, 1], [-size, 0, size]);
 
         return {
             transform: [
