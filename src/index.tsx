591f0d89744d4f7b97784139c86fa1ef85a70606
赵東澔 <zhaodonghao586@outlook.com>
2022-06-04 11:03:30 +0800

    fix: global.__reanimatedWorkletInit is not a function
    fix #159, fix #182

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+if (!('__reanimatedWorkletInit' in global)) {
+    Object.assign(global, { __reanimatedWorkletInit: () => {} });
+}
+
 export type { TCarouselProps, ICarouselInstance } from './types';
 import Carousel from './Carousel';
 
