a3ee957bb70f3bf59e73ee75f90648cd433fc1fb
Caspian <zhaodonghao586@outlook.com>
2023-11-26 23:00:30 +0000

    docs: update doc website

--- example/website/theme.config.tsx
+++ example/website/theme.config.tsx
@@ -3,7 +3,10 @@ import React from "react";
 import type { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>RNRC 🎠</span>,
+  logo: <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+    <img src="/site-icon.png" alt="RNRC Logo" width={24} height={24} style={{ marginRight: "8px" }} />
+    <span style={{ fontWeight: 700, fontSize: "22px" }}>RNRC</span>
+  </div>,
   project: {
     link: "https://github.com/dohooo/react-native-reanimated-carousel",
   },
@@ -12,9 +15,22 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "Nextra Docs Template",
+    text: "Copyright © 2023 Caspian. Built with Nextra.",
   },
   darkMode: true,
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – RNRC",
+    };
+  },
+  head: (
+    <>
+      <link rel="shortcut icon" href="/site-icon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="react-native-reanimated-carousel" />
+      <meta property="og:description" content="A performant carousel for React Native powered by Reanimated." />
+    </>
+  ),
 };
 
 export default config;
