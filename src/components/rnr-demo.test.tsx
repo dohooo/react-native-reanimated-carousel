333008f59bc67e1d1ed0f980df9d476af8e0fae9
Caspian <zhaodonghao586@outlook.com>
2023-12-04 18:40:47 +0000

    chore: setup test env

---
+++ src/components/rnr-demo.test.tsx
@@ -0,0 +1,43 @@
+import type { FC } from "react";
+import React from "react";
+import Animated, { useAnimatedStyle, useDerivedValue } from "react-native-reanimated";
+import renderer from "react-test-renderer";
+
+describe("useSharedValue", () => {
+  it("retains value on rerender", () => {
+    const initialValue = 0;
+    const updatedValue = 1;
+
+    interface Props {
+      key: string
+      value: number
+    }
+
+    const TestComponent: FC<Props> = (props) => {
+      const opacity = useDerivedValue(() => props.value, [props.value]);
+      const animatedStyle = useAnimatedStyle(() => ({
+        opacity: opacity.value,
+      }), [opacity]);
+
+      return <Animated.View style={animatedStyle} />;
+    };
+
+    // When rendering with initial value
+    const wrapper = renderer.create(<TestComponent key="box" value={initialValue} />);
+
+    expect(
+      typeof wrapper.root.children[0] !== "string"
+        ? wrapper.root.children[0].props.style.animatedStyle.current.value.opacity
+        : false,
+    ).toBe(initialValue);
+
+    // When rendering with updated value
+    wrapper.update(<TestComponent key="box" value={updatedValue} />);
+
+    expect(
+      typeof wrapper.root.children[0] !== "string"
+        ? wrapper.root.children[0].props.style.animatedStyle.current.value.opacity
+        : false,
+    ).toBe(initialValue);
+  });
+});
