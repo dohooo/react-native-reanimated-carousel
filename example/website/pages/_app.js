73ef6984074170260136c8bedc6c9a765669e105
Caspian Zhao <caspian.zhao@outlook.com>
2024-10-14 06:21:36 -0700

    Add posthog

--- example/website/pages/_app.js
+++ example/website/pages/_app.js
@@ -1,11 +1,26 @@
 import "../styles.css";
-import { Analytics } from "@vercel/analytics/react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-export default function MyApp({ Component, pageProps }) {
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host:
+      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
+
+export default function MyApp({
+  Component,
+  pageProps: { session: _session, ...pageProps },
+}) {
   return (
-    <>
+    <PostHogProvider client={posthog}>
       <Component {...pageProps} />
-      <Analytics />
-    </>
+    </PostHogProvider>
   );
 }
