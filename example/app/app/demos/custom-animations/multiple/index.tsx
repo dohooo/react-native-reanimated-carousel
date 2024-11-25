ec99e73a88cf1a421d7b44a69c6a8696fd9dc4c1
Caspian Zhao <caspian.zhao@outlook.com>
2024-11-25 17:33:31 +0800

    docs: expo-dev-client supported in example app

--- example/app/app/demos/custom-animations/multiple/index.tsx
+++ example/app/app/demos/custom-animations/multiple/index.tsx
@@ -1,72 +1,55 @@
 import * as React from "react";
 import { View } from "react-native";
-import Carousel from "react-native-reanimated-carousel";
+import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
 
 import { SBItem } from "@/components/SBItem";
-import SButton from "@/components/SButton";
-import { ElementsText, window } from "@/constants/sizes";
+import { window } from "@/constants/sizes";
 import { CaptureWrapper } from "@/store/CaptureProvider";
+import { CarouselAdvancedSettingsPanel } from "@/components/CarouselAdvancedSettingsPanel";
+import { useAdvancedSettings } from "@/hooks/useSettings";
+import { defaultDataWith6Colors } from "@/components/CarouselBasicSettingsPanel";
 
 const PAGE_WIDTH = window.width;
 
-const COUNT = 6;
+const COUNT = 4;
 
 function Index() {
-  const [isVertical, setIsVertical] = React.useState(false);
-  const [isFast, setIsFast] = React.useState(false);
-  const [isAutoPlay, setIsAutoPlay] = React.useState(false);
-
-  const baseOptions = isVertical
-    ? ({
-        vertical: true,
-        width: PAGE_WIDTH,
-        height: PAGE_WIDTH / 2 / COUNT,
-        style: {
-          height: PAGE_WIDTH / 2,
-        },
-      } as const)
-    : ({
-        vertical: false,
-        width: PAGE_WIDTH / COUNT,
-        height: PAGE_WIDTH / 2,
-        style: {
-          width: PAGE_WIDTH,
-        },
-      } as const);
+  const ref = React.useRef<ICarouselInstance>(null);
+  const { advancedSettings, onAdvancedSettingsChange } = useAdvancedSettings({
+    // These values will be passed in the Carousel Component as default props
+    defaultSettings: {
+      autoPlay: false,
+      autoPlayInterval: 2000,
+      autoPlayReverse: false,
+      data: defaultDataWith6Colors,
+      height: 258,
+      loop: true,
+      pagingEnabled: true,
+      snapEnabled: true,
+      vertical: false,
+      width: PAGE_WIDTH / COUNT,
+    },
+  });
 
   return (
     <View style={{ flex: 1 }}>
       <CaptureWrapper>
         <Carousel
-          {...baseOptions}
-          loop
-          autoPlay={isAutoPlay}
-          autoPlayInterval={isFast ? 100 : 2000}
-          data={[...new Array(12).keys()]}
+          ref={ref}
+          {...advancedSettings}
+          loop={false}
+          overscrollEnabled={false}
+          style={{ width: PAGE_WIDTH }}
+          data={[...new Array(6).keys()]}
           renderItem={({ index }) => <SBItem key={index} index={index} />}
         />
       </CaptureWrapper>
-      <SButton
-        onPress={() => {
-          setIsVertical(!isVertical);
-        }}
-      >
-        {isVertical ? "Set horizontal" : "Set Vertical"}
-      </SButton>
-      <SButton
-        onPress={() => {
-          setIsFast(!isFast);
-        }}
-      >
-        {isFast ? "NORMAL" : "FAST"}
-      </SButton>
-      <SButton
-        onPress={() => {
-          setIsAutoPlay(!isAutoPlay);
-        }}
-      >
-        {ElementsText.AUTOPLAY}:{`${isAutoPlay}`}
-      </SButton>
+
+      <CarouselAdvancedSettingsPanel
+        carouselRef={ref}
+        advancedSettings={advancedSettings}
+        onAdvancedSettingsChange={onAdvancedSettingsChange}
+      />
     </View>
   );
 }
