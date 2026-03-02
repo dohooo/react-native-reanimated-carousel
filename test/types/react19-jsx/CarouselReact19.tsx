1a1813360f755b1415c00943980d62db3f5d0185
Caspian Zhao <caspian.zhao@outlook.com>
2026-03-02 16:47:42 +0800

    fix(types): resolve React 19 JSX incompatibility for Carousel

---
+++ test/types/react19-jsx/CarouselReact19.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import Carousel from "../../../src";
+
+const carouselElement = (
+  <Carousel
+    width={300}
+    height={200}
+    data={[1, 2, 3]}
+    renderItem={() => <></>}
+  />
+);
+
+type IsExact<A, B> = (<T>() => T extends A ? 1 : 2) extends <T>() => T extends B ? 1 : 2
+  ? (<T>() => T extends B ? 1 : 2) extends <T>() => T extends A ? 1 : 2
+    ? true
+    : false
+  : false;
+
+type Assert<T extends true> = T;
+type CarouselReturn = ReturnType<typeof Carousel<number>>;
+const assertCarouselReturnType: Assert<IsExact<CarouselReturn, React.ReactNode>> = true;
+
+void carouselElement;
+void assertCarouselReturnType;
